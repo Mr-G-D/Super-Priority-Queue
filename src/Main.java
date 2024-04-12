97bb14635f168acec002598e3cc796304c38443a
Mr-G-D <daredinesh00@gmail.com>
2024-04-12 17:48:27 -0400

    main function :alembic:

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 
 /**
  * @author DINESH KUMAR
- */// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Heap heap = new Heap("minheap");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        heap.insert(10);
+        heap.insert(20);
+        heap.insert(30);
+        heap.insert(40);
+        heap.insert(50);
+        heap.insert(5);
+        heap.insert(60);
+        heap.insert(70);
+        heap.insert(80);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        heap.printTreein2D();
     }
 }
