efc6bfce82c64baeae61fe821be586765b7d0b52
Sanjay Bharathi <sanjaystz98@gmail.com>
2024-04-15 01:40:19 -0400

    Added cases

--- src/Main.java
+++ src/Main.java
@@ -29,10 +29,57 @@ public class Main {
 
         heap.printTreein2D();
         System.out.println("_______________________________________");
-        int temp = heap.replaceKey(new Entry(40, 10), 400);
-        System.out.println(temp);
+
+        heap.removeTop();
         heap.printTreein2D();
-
-
+        System.out.println("---------------------------------------");
+        heap.remove(new Entry(40, 10));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.insert(new Entry(40, 10));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceKey(new Entry(40, 10), 400);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.insert(new Entry(400, 10));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceKey(new Entry(400, 10), 25);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceVal(new Entry(90, 10), 50);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceVal(new Entry(90, 10), 50);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.toggle();
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.insert(new Entry(40, 10));
+        heap.insert(new Entry(500, 20));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.remove(new Entry(40, 10));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.insert(new Entry(40, 10));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceKey(new Entry(40, 10), 400);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.insert(new Entry(400, 10));
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceKey(new Entry(400, 10), 25);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceVal(new Entry(90, 10), 50);
+        heap.printTreein2D();
+        System.out.println("---------------------------------------");
+        heap.replaceVal(new Entry(90, 10), 50);
+        heap.printTreein2D();
     }
 }
